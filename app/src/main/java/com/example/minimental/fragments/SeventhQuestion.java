2a9a868868ad650446c2986c26a5f57083b34f89
Amit-proj <amitsalman80@gmail.com>
2022-03-26 17:09:03 +0300

    Seventh screen

--- app/src/main/java/com/example/minimental/fragments/SeventhQuestion.java
+++ app/src/main/java/com/example/minimental/fragments/SeventhQuestion.java
@@ -2,9 +2,11 @@ package com.example.minimental.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,11 +17,17 @@ import com.example.minimental.R;
 
 public class SeventhQuestion extends Fragment {
 
+    ImageView milk;
+    float x = 0, y = 0;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.seventh_question,container,false);
         Button nxtBtn = rootView.findViewById(R.id.next_Btn);
+
+        milk = (ImageView) rootView.findViewById(R.id.seventh_question_ImageView_milk);
+        milk.setOnTouchListener(new MyTouchListener());
+
         nxtBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,4 +38,31 @@ public class SeventhQuestion extends Fragment {
 
         return rootView;
     }
+
+    final class MyTouchListener implements View.OnTouchListener{
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+            switch (event.getActionMasked()){
+                case MotionEvent.ACTION_DOWN:
+                    x = event.getX();
+                    y = event.getY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    float movedX, movedY;
+                    movedX = event.getX();
+                    movedY = event.getY();
+
+                    float distanceX = movedX - x;
+                    float distanceY = movedY - y;
+
+                    view.setX(view.getX()+distanceX);
+                    view.setY(view.getY()+distanceY);
+
+                    break;
+            }
+
+            return true;
+        }
+    }
+
 }
