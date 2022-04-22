c162bba93e75449b8866a632f4d14d8e9cb1b339
David-Shmidt <davidshmidt@gmail.com>
2022-04-22 20:48:31 +0300

    information fragment speech recognition

--- app/src/main/java/com/example/minimental/fragments/MathVersion.java
+++ app/src/main/java/com/example/minimental/fragments/MathVersion.java
@@ -1,6 +1,8 @@
 package com.example.minimental.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,42 +10,54 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.widget.TextViewCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
-import com.example.minimental.LifeCycleObserver;
 import com.example.minimental.R;
-import com.example.minimental.ViewModels.IResultHandler;
 import com.example.minimental.ViewModels.SharedViewModel;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
 
-public class MathVersion extends Fragment implements IResultHandler {
+import java.util.ArrayList;
+
+public class MathVersion extends Fragment  {
     private SharedViewModel sharedViewModel;
-    private LifeCycleObserver speechRecognitionObserver;
+    private ActivityResultLauncher<Intent> speechRecognizerLauncher;
     private Observer<String> getAnswerObserver;
     private TextView resultText;
 
-    /*@Override
+    @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
-        speechRecognitionObserver = new LifeCycleObserver(requireActivity().getActivityResultRegistry());
-        getLifecycle().addObserver(speechRecognitionObserver);
-        //speechRecognitionObserver.setMyFragmentViewModel(sharedViewModel);
+        speechRecognizerLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult activityResult) {
+                Intent data = activityResult.getData();
+                StringBuffer speechResult = new StringBuffer();
+                ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                for(String r:results)
+                {
+                    speechResult.append(r);
+                }
+                String result = speechResult.toString();
+                sharedViewModel.setMathAnswerGiven(result);
+            }
+        });
         getAnswerObserver = new Observer<String>() {
             @Override
             public void onChanged(String s) {
                 resultText.setText(s);
             }
         };
-        sharedViewModel.getAnswerGiven().observe(this , getAnswerObserver);
-    }*/
+        sharedViewModel.getMathAnswerGiven().observe(this , getAnswerObserver);
+    }
 
     @Nullable
     @Override
@@ -51,25 +65,27 @@ public class MathVersion extends Fragment implements IResultHandler {
         View rootView = inflater.inflate(R.layout.math_version,container,false);
         Button nxtBtn = rootView.findViewById(R.id.next_Btn);
         ImageButton speechBtn = rootView.findViewById(R.id.mic_image_btn);
-        resultText = rootView.findViewById(R.id.result_txt_math);
-        nxtBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Navigation.findNavController(view).navigate(R.id.action_mathVersion_to_fourthQuestion);
-            }
-        });
+        resultText = rootView.findViewById(R.id.math_version_question_textview);
         speechBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                speechRecognitionObserver.activateSpeechRecognition();
+                startSpeechRecognition();
+            }
+        });
+
+        nxtBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_mathVersion_to_fourthQuestion);
             }
         });
         return rootView;
     }
-
-    @Override
-    public void handleResult(String result) {
-        sharedViewModel.setAnswerGiven(result);
+    private void startSpeechRecognition()
+    {
+        Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_RESULTS , 1);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE , "en");
+        speechRecognizerLauncher.launch(speechIntent);
     }
 }
