ca947a6e14070d87eebbadedf25678b554587d0f
Amit-proj <amitsalman80@gmail.com>
2022-04-24 19:15:26 +0300

    UI updates

--- app/src/main/java/com/example/minimental/fragments/ChooseThirdQuestion.java
+++ app/src/main/java/com/example/minimental/fragments/ChooseThirdQuestion.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.Navigation;
@@ -15,26 +16,25 @@ import androidx.navigation.Navigation;
 import com.example.minimental.R;
 
 public class ChooseThirdQuestion extends Fragment {
-
     @Override
     public void onStart() {
         super.onStart();
     }
-
+    private CardView mathCard, spellingCard;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.choose_third_question,container,false);
-        Button nxtMathBtn = rootView.findViewById(R.id.button_of_mathQuestion);
-        nxtMathBtn.setOnClickListener(new View.OnClickListener() {
+         mathCard = rootView.findViewById(R.id.math_card);
+        mathCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Navigation.findNavController(view).navigate(R.id.action_chooseThirdQuestion_to_mathVersion);
             }
         });
-        Button nxtSpellBtn = rootView.findViewById(R.id.button_of_spellingQuestion);
-        nxtSpellBtn.setOnClickListener(new View.OnClickListener() {
+        spellingCard = rootView.findViewById(R.id.spelling_card);
+        spellingCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
