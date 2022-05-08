ccda7daed375b433f92db96b3bd22b3fcc7e7bff
David-Shmidt <davidshmidt@gmail.com>
2022-05-08 09:14:01 +0300

    speech recognition in sixth question

--- app/src/main/java/com/example/minimental/fragments/Sixth_question.java
+++ app/src/main/java/com/example/minimental/fragments/Sixth_question.java
@@ -1,49 +1,93 @@
 package com.example.minimental.fragments;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageButton;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.Observer;
 import androidx.navigation.Navigation;
 
 import com.example.minimental.MainActivityViewModel;
 import com.example.minimental.R;
-import com.example.minimental.SixthQuestion;
 import com.example.minimental.ViewModels.SharedViewModel;
+
+import java.util.ArrayList;
 
 public class Sixth_question extends Fragment {
 
 
     private SharedViewModel sharedViewModel;
-    private SixthQuestion sixthQuestion = new SixthQuestion();
-    private EditText currentSentence;
-
+    private ActivityResultLauncher<Intent> speechRecognizerLauncher;
+    private Observer<String> getAnswerObserver;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        speechRecognizerLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult activityResult) {
+                Intent data = activityResult.getData();
+                if (data != null) {
+                    StringBuffer speechResult = new StringBuffer();
+                    if (data != null) {
+                        ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                        for (String r : results) {
+                            speechResult.append(r);
+                        }
+                        String result = speechResult.toString();
+                        //sharedViewModel.setRepeatedSentence(result);
+                    }
+                }
+            }
+        });
+/*        getAnswerObserver = new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                resultText.setText(s);
+            }
+        };
+        sharedViewModel.getMathAnswerGiven().observe(this , getAnswerObserver);*/
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.sixth_question,container,false);
-        sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
-        currentSentence = rootView.findViewById(R.id.input_SentenceET);
         Button nxtBtn = rootView.findViewById(R.id.next_Btn);
+        ImageButton speechBtn = rootView.findViewById(R.id.image_of_microphone);
+        speechBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSpeechRecognition();
+            }
+        });
         nxtBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                //sixthQuestion.setSentence(currentSentence.getText().toString());
-                //sharedViewModel.setRepeatedSentence(sixthQuestion);
                 Navigation.findNavController(view).navigate(R.id.action_sixth_question_to_SeventhQuestion);
             }
         });
         return rootView;
 
+    }
+
+    private void startSpeechRecognition()
+    {
+        Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_RESULTS , 1);
+        speechRecognizerLauncher.launch(speechIntent);
     }
 }
